41efa301fa97842cc349f4692f4df072602d67ad
gargVader <girishgargcool@gmail.com>
2021-07-01 18:35:41 +0530

    onCLick not working

--- app/src/main/java/com/example/viewoverlay/MainActivity.java
+++ app/src/main/java/com/example/viewoverlay/MainActivity.java
@@ -6,31 +6,31 @@ import androidx.appcompat.content.res.AppCompatResources;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroupOverlay;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import static android.view.View.MeasureSpec.makeMeasureSpec;
+import static android.view.View.MeasureSpec.EXACTLY;
+
 public class MainActivity extends AppCompatActivity {
 
     private ViewGroup root;
     private Button button;
-    private Drawable hand;
-    private View handPointer;
-    RelativeLayout.LayoutParams handPointerParam;
+//    private Drawable hand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        button = findViewById(R.id.button);
         root = findViewById(android.R.id.content);
-        handPointer = getLayoutInflater().inflate(R.layout.hand_view, null);
-
-        hand = AppCompatResources.getDrawable(getApplicationContext(), R.drawable.hand);
+        button = findViewById(R.id.button);
+//        hand = AppCompatResources.getDrawable(getApplicationContext(), R.drawable.hand);
 
         // We need to delay this until everything is actually measured and laid
         // out on-screen, which doesn't happen until after onCreate() ends.
@@ -54,18 +54,39 @@ public class MainActivity extends AppCompatActivity {
         // of the Button. The index finger isn't centered in the image, though, so it looks
         // off, but this is just for a quick demo. Also just using the image at its
         // actual size, which is pretty huge.
-        final int handLeft = buttonBoundsInRoot.centerX() - hand.getIntrinsicWidth() / 2;
-        final int handTop = buttonBoundsInRoot.centerY();
-
-        hand.setBounds(handLeft, handTop, handLeft + hand.getIntrinsicWidth(), handTop + hand.getIntrinsicHeight());
-        handPointerParam = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT);
-        handPointerParam.leftMargin = handLeft;
-        handPointerParam.topMargin = handTop;
-        handPointerParam.rightMargin = handLeft + hand.getIntrinsicWidth();
-        handPointerParam.bottomMargin = handTop + hand.getIntrinsicHeight();
-
+//        final int handLeft = buttonBoundsInRoot.centerX() - hand.getIntrinsicWidth() / 2;
+//        final int handTop = buttonBoundsInRoot.centerY();
+//        hand.setBounds(handLeft, handTop, handLeft + hand.getIntrinsicWidth(), handTop + hand.getIntrinsicHeight());
 //        rootOverlay.add(hand);
-        rootOverlay.add(handPointer);
+
+        // The layout_width and _height in the XML are not applied when inflating
+        // a View like this, so this example figures the pixel values here.
+        final int handWidth = dpToPx(60);
+        final int handHeight = dpToPx(70);
+
+        final View handView = getLayoutInflater().inflate(R.layout.hand_view, null);
+        final int handLeft = buttonBoundsInRoot.centerX() - handWidth / 2;
+        final int handTop = buttonBoundsInRoot.centerY();
+        final int handRight = buttonBoundsInRoot.centerX() + handWidth / 2;
+        final int handBottom = buttonBoundsInRoot.centerY() + handHeight;
+
+        handView.measure(makeMeasureSpec(handWidth, EXACTLY), makeMeasureSpec(handHeight, EXACTLY));
+        handView.layout(handLeft, handTop, handRight, handBottom);
+
+        handView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Main", "onClick: ");
+            }
+        });
+
+        rootOverlay.add(handView);
+
+        // DEBUG - Just so you can see how it lines up.
+        handView.setBackgroundColor(0x55ff00ff);
+    }
+
+    int dpToPx(float dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
     }
 }
